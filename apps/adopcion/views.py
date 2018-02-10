777cc61c4e9de927f6e242875fac357ec61070cd
kike <mt16007@ues.edu.sv>
2018-02-09 22:17:43 -0600

    Se inicio el proyecto en video 9

---
+++ apps/adopcion/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index_adopcion(request):
+	return HttpResponse("I'm the principal page of the adoption's apps")
