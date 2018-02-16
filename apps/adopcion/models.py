92c25e301135f2fed6286043811b2b16e05cb23f
kike <mt16007@ues.edu.sv>
2018-02-16 07:17:45 -0600

    Video 17 CRUD con dos formularios Parte 1

--- apps/adopcion/models.py
+++ apps/adopcion/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 # Create your models here.
 
 
@@ -15,3 +14,12 @@ class Persona(models.Model):
 
 	def __str__(self):
 		return '{} {}'.format(self.nombre, self.apellido)
+
+
+class Solicitud(models.Model):
+	persona = models.ForeignKey(Persona, null=True, blank=True,on_delete=models.CASCADE)
+	numero_mascotas = models.IntegerField()
+	razones = models.TextField()
+
+
+
--- refugio/urls.py
+++ refugio/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
-app_name="mascota"
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
